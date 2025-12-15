ad0da3adbc3a49f28a99b7ac1ab2b3f04d90cd7d
Devollox <122895078+Devollox@users.noreply.github.com>
2025-12-15 19:18:26 +0300

    Update index.tsx

--- components/list/index.tsx
+++ components/list/index.tsx
@@ -4,13 +4,17 @@ import styles from './list.module.scss'
 type ListProps = {
   listTitle: string
   children: ReactNode
+  badge?: string
 }
 
-const List = ({ listTitle, children }: ListProps) => {
+const List = ({ listTitle, children, badge }: ListProps) => {
   return (
-    <div>
-      <h2 className={styles.title_block}>{listTitle}</h2>
-      {children}
+    <div className={styles.list_section}>
+      <div className={styles.list_header}>
+        <h3 className={styles.title_block}>{listTitle}</h3>
+        <span className={styles.list_badge}>{badge}</span>
+      </div>
+      <div>{children}</div>
     </div>
   )
 }
